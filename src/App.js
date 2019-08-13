e4ac7a99b0e9652d36f6fb5024594540d8c3d02a
paperlambda <revzankamarullah@gmail.com>
2019-08-14 00:32:45 +0700

    Setup application router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react'
+import { Router, Route, Switch } from 'react-router-dom'
 import Catalog from '@/pages/Catalog/Catalog'
 import ErrorBoundary from '@/containers/ErrorBoundary'
 import '@/helpers/firebase'
-import NavigationTop from '@/containers/NavigationTop'
+import history from '@/helpers/history'
 
 const App = () => (
   <ErrorBoundary>
-    <NavigationTop />
-    <Catalog />
+    <Router history={history}>
+      <Switch>
+        <Route exact={true} path="/" component={Catalog}/>
+      </Switch>
+    </Router>
   </ErrorBoundary>
 )
 
--- src/pages/Catalog/Catalog.js
+++ src/pages/Catalog/Catalog.js
@@ -8,6 +8,7 @@ import ProductCard from './containers/ProductCard'
 import FilterModal from '@/pages/Catalog/containers/FilterModal'
 import * as catalogService from '@/services/catalog-service'
 import LoadingIndicator from '@/components/LoadingIndicator'
+import NavigationTop from '@/containers/NavigationTop'
 
 const Catalog = () => {
   const [showFilterModal, setFilterModal] = React.useState(false)
@@ -43,36 +44,36 @@ const Catalog = () => {
     }
   }
 
-  if (isLoading) {
-    return (
-      <Main>
-        <LoadingIndicator/>
-      </Main>
-    )
-  }
-
   return (
     <Main>
-      <Container>
-        <Card>
-          <FilterOptions>
-            <Flex jc="flex-start" ai="flex-end">
-              <select value={sortBy} onChange={(e) => _didSelectSortOpt(e)} name="sort">
-                <option value="" style={{ display: 'none' }}>URUTKAN</option>
-                <option value="created|desc">Terbaru</option>
-                <option value="price|asc">Termurah</option>
-                <option value="price|desc">Termahal</option>
-              </select>
-              <Button color="secondary" onClick={() => _toggleShowModal()}>FILTER</Button>
-            </Flex>
-          </FilterOptions>
-          <div>
-            {
-              products && products.map((product) => (<ProductCard product={product} key={product.id}/>))
-            }
-          </div>
-        </Card>
-      </Container>
+      <NavigationTop/>
+      {
+        !isLoading && (
+          <Container>
+            <Card>
+              <FilterOptions>
+                <Flex jc="flex-start" ai="flex-end">
+                  <select value={sortBy} onChange={(e) => _didSelectSortOpt(e)} name="sort">
+                    <option value="" style={{ display: 'none' }}>URUTKAN</option>
+                    <option value="created|desc">Terbaru</option>
+                    <option value="price|asc">Termurah</option>
+                    <option value="price|desc">Termahal</option>
+                  </select>
+                  <Button color="secondary" onClick={() => _toggleShowModal()}>FILTER</Button>
+                </Flex>
+              </FilterOptions>
+              <div>
+                {
+                  products && products.map((product) => (<ProductCard product={product} key={product.id}/>))
+                }
+              </div>
+            </Card>
+          </Container>
+        )
+      }
+      {
+        isLoading && <LoadingIndicator/>
+      }
       { showFilterModal && (<FilterModal willClose={() => _toggleShowModal()}/>) }
     </Main>
   )
